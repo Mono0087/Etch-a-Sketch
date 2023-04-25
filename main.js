7f4d28906660618d7085a59c282662b96974d497
Mono <125167202+Mono0087@users.noreply.github.com>
2023-04-26 03:37:42 +0500

    Add "Set amount" and "Reset" buttons

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 const sketchField = document.querySelector('.sketch_field');
+const setBtn = document.querySelector('.btn-set');
+const resetBtn = document.querySelector('.btn-reset');
+const amountInput = document.querySelector('#amount')
+let amount = +amountInput.value;
 
-let divNumber = 16;
 
-// Get width and height for one "pixel" of the grid
-pixelWidth = (sketchField.getBoundingClientRect().width - 4) / divNumber + 'px';
-pixelHeight = (sketchField.getBoundingClientRect().width - 4) / divNumber + 'px';
 
 // Add div's to sketch field
-for (let i = 0; i < divNumber * divNumber; i++) {
-    let div = document.createElement('div');
-    div.classList.add('field_div');
-    sketchField.append(div);
-    div.style.border = "1px solid rgb(100, 71, 71)";
-    div.style.background = "white"
-    div.style.width = pixelWidth;
-    div.style.height = pixelHeight;
+function getGrid(amount) {
+    // Get width and height for one "pixel" of the grid
+    pixelWidth = (sketchField.getBoundingClientRect().width - 4) / amount + 'px';
+    pixelHeight = (sketchField.getBoundingClientRect().width - 4) / amount + 'px';
+    for (let i = 0; i < amount * amount; i++) {
+        let div = document.createElement('div');
+        div.classList.add('field_div');
+        sketchField.append(div);
+    };
+    // Set pixels style and grid
+    let pixels = document.querySelectorAll('.field_div')
+    Array.from(pixels).forEach((item) => {
+        item.style.background = "white"
+        item.style.border = "1px solid rgb(100, 71, 71)";
+        item.style.height = pixelHeight;
+        item.style.width = pixelWidth;
+    });
 }
+getGrid(+amount);
 
 // Add hover effect
 sketchField.addEventListener('mousemove', changeColor);
@@ -23,4 +33,24 @@ function changeColor(e) {
     if (e.target.classList[0] == "field_div") {
         e.target.style.backgroundColor = 'black'
     }
-}
+};
+
+// Set pixels amount
+setBtn.addEventListener('click', () => {
+    let newAmount = amountInput.value;
+    console.log(newAmount)
+    if (newAmount && Number.isInteger(+newAmount) && +newAmount >= 4 && +newAmount <= 100) {
+        newAmount = +amountInput.value;
+        sketchField.innerHTML = '';
+        getGrid(+newAmount);
+    } else {
+        alert('Amount must be a numeric value less than 100 and greater than 4');
+    }
+});
+
+// Reset grid
+resetBtn.addEventListener('click', () => {
+    sketchField.innerHTML = '';
+    getGrid(16);
+    amountInput.value = '16';
+});
