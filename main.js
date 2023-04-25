b63849899b64302edff7a95cc8a50386b7afdc2c
Mono <125167202+Mono0087@users.noreply.github.com>
2023-04-26 02:13:13 +0500

    Add faster grid construction

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const sketchField = document.querySelector('.sketch_field');
 
-let divNumber = 20;
+let divNumber = 16;
+
+// Get width and height for one "pixel" of the grid
+pixelWidth = (sketchField.getBoundingClientRect().width - 4) / divNumber + 'px';
+pixelHeight = (sketchField.getBoundingClientRect().width - 4) / divNumber + 'px';
 
 // Add div's to sketch field
 for (let i = 0; i < divNumber * divNumber; i++) {
     let div = document.createElement('div');
     div.classList.add('field_div');
-    sketchField.append(div)
+    sketchField.append(div);
     div.style.border = "1px solid rgb(100, 71, 71)";
     div.style.background = "white"
-    div.style.width = (sketchField.getBoundingClientRect().width - 4) / divNumber + 'px';
-    div.style.height = (sketchField.getBoundingClientRect().width - 4) / divNumber + 'px';
+    div.style.width = pixelWidth;
+    div.style.height = pixelHeight;
 }
 
 // Add hover effect
