fd836315af02c78a5a78f76e719c65131ae36f16
Mono <125167202+Mono0087@users.noreply.github.com>
2023-04-26 02:07:21 +0500

    Add hover effect

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const sketchField = document.querySelector('.sketch_field');
 
-let divNumber = 16;
+let divNumber = 20;
 
 // Add div's to sketch field
 for (let i = 0; i < divNumber * divNumber; i++) {
@@ -13,3 +13,10 @@ for (let i = 0; i < divNumber * divNumber; i++) {
     div.style.height = (sketchField.getBoundingClientRect().width - 4) / divNumber + 'px';
 }
 
+// Add hover effect
+sketchField.addEventListener('mousemove', changeColor);
+function changeColor(e) {
+    if (e.target.classList[0] == "field_div") {
+        e.target.style.backgroundColor = 'black'
+    }
+}
